668bbc4dbe9fbde86a03202040b7e02cc5f6e656
KolesnichenkoAG <kag.1981@mail.ru>
2022-12-25 16:02:06 +0600

    ДЗ урок 7

--- lesson7_HW_springData/src/main/java/ru/geekbrains/Application.java
+++ lesson7_HW_springData/src/main/java/ru/geekbrains/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lesson7HwSpringDataApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lesson7HwSpringDataApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
